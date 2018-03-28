1c4f4ee976fd3ce5ee708a0c2425908080d8aac7
Matthew Flinders <ucapmfl@ucl.ac.uk>
2018-03-28 17:50:00 +0100

    Add main project file

---
+++ cqo/main.py
@@ -0,0 +1,21 @@
+
+
+# OPTIONS !!! PUT THESE IN A CFG/TURN THESE INTO ARGUMENTS !!!
+N = 2
+LAMBDA = 0.1
+SIM_DURATION = 100
+COIN_OP = [
+    [1, 1],
+    [1, -1]
+]
+
+
+# Prepare basis & initial state
+
+# Iterate over walk steps
+#   - apply coin
+#   - apply shift
+
+# Output statistics
+#   - pdf
+#   - entanglement
