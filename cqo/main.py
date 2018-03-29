241467d797f641711d495a41b2114632f992ae04
Matthew Flinders <ucapmfl@ucl.ac.uk>
2018-03-29 19:34:03 +0100

    Fix missing np.

--- cqo/main.py
+++ cqo/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # OPTIONS !!! PUT THESE IN A CFG/TURN THESE INTO ARGUMENTS !!!
 N = 2
 LAMBDA = 0.1
-SIM_DURATION = 50
+SIM_DURATION = 5
 COIN_OP = [
     [1, 1],
     [1, -1]
@@ -21,23 +21,23 @@ up_projector = [
     [1, 0],
     [0, 0]
 ]
-down_projector = eye(2) - up_projector
+down_projector = np.eye(2) - up_projector
 
 spin_state = up_projector
 
-particle_state = zeros(accessed_state_count, accessed_state_count)
+particle_state = np.zeros(accessed_state_count, accessed_state_count)
 particle_state[0][0] = 1
 
-state = kron(spin_state, particle_state)
+state = np.kron(spin_state, particle_state)
 
-coin_op = kron(COIN_OP, identity(accessed_state_count))
+coin_op = np.kron(COIN_OP, np.eye(accessed_state_count))
 
-translate_op = zeros(accessed_state_count)
+translate_op = np.zeros(accessed_state_count)
 for i in range(accessed_state_count):
     translate_op[i][i-1] = 1
 
-shift_op = kron(up_projector, eye(accessed_state_count))
-            + kron(down_projector, translate_op)
+shift_op = np.kron(up_projector, np.eye(accessed_state_count)) + \
+             np.kron(down_projector, translate_op)
 
 # Iterate over walk steps
 for i in range(N*SIM_DURATION):
@@ -55,4 +55,4 @@ for i in range(N*SIM_DURATION):
 #   - entanglement
 
 plt.plot(np.diag(state))
-plt.savefig('C:\Users\0912 esmé\PhD\programming\coined-quantum-oscillator\output')
+plt.savefig("C:\outputpdf.png")
