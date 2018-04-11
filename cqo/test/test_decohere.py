903f6edd68074e19dc2041f087db0f16c034451b
Matthew Flinders <ucapmfl@ucl.ac.uk>
2018-04-11 18:37:01 +0100

    Add unit tests for simulate.decohere

---
+++ cqo/test/test_decohere.py
@@ -0,0 +1,17 @@
+import unittest
+from simulate import decohere
+import numpy as np
+
+class TestDecohere(unittest.TestCase):
+
+    # gamma=0 is identity
+    def test_no_decoherence(self):
+        state = np.random.randn((10,10))
+
+        final_state = decohere(state, 0)
+
+        self.assertTrue(np.allclose(state, final_state))
+
+    def test_no_negative_gamma(self):
+        state = np.eye(6)
+        self.assertRaises(ValueError, decohere, state, -1.0)
