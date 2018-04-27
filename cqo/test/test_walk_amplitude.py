9236554326a9d8f6e28ba42e55fc1848e7efaf87
Matthew Flinders <ucapmfl@ucl.ac.uk>
2018-04-27 16:28:19 +0100

    Fix walk amplitude tests

--- cqo/test/test_walk_amplitude.py
+++ cqo/test/test_walk_amplitude.py
@@ -12,7 +12,7 @@ class TestWalkAmplitudes(unittest.TestCase):
         # N < 1 should raise
 
         for N in range(-1, 1):
-            self.assertRaises(ValueError, walk_amplitudes, N)
+            self.assertRaises(ValueError, walk_amplitudes, N, self.I)
 
 
     def test_identity_coin_op(self):
@@ -20,7 +20,9 @@ class TestWalkAmplitudes(unittest.TestCase):
 
         N = 4
 
-        expected_output = np.array([[0,0]*N/2, [1,0], [0,0]*N/2])
+        L = R = 2
+
+        expected_output = np.array([[0,0]]*L +  [[1,0]] + [[0,0]]*R)
 
         true_output = walk_amplitudes(N, self.I)
 
@@ -29,20 +31,24 @@ class TestWalkAmplitudes(unittest.TestCase):
 
     def test_coin_op_must_be_2x2(self):
 
-        self.assertRaises(ValueError, walk_amplitude, 4, np.eye(4))
+        self.assertRaises(ValueError, walk_amplitudes, 4, np.eye(4))
 
 
     def test_coin_op_unitary(self):
 
         bad_coin_op = np.array([[1,0], [0,0]])
 
-        self.assertRaises(ValueError, walk_amplitude, 4, bad_coin_op)
+        self.assertRaises(ValueError, walk_amplitudes, 4, bad_coin_op)
 
 
     def test_output_length(self):
 
-        for i in range(10):
+        for i in range(1,10):
 
             output = walk_amplitudes(i, self.H)
 
-            self.assertTrue(output.shape == (2, i+1))
+            self.assertTrue(output.shape == (i+1, 2))
+
+
+if __name__ == "__main__":
+    unittest.main()
