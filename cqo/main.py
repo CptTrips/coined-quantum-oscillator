173a85c0037d48ef48924bebf375474374da2009
Matthew Flinders <ucapmfl@ucl.ac.uk>
2018-07-05 13:26:28 +0100

    Change main program to use new calculation method

--- cqo/main.py
+++ cqo/main.py
@@ -1,13 +1,15 @@
 import numpy as np
-import simulate
+from simulation import final_state, CoherentState, SpinState, ThermalState, final_state_recursive
+from algebra import projector
+import units
 import output
 
 
 
 def main():
 
+    #parser = argparse.ArgumentParser()
     """
-    parser = argparse.ArgumentParser()
 
     parser.add_argument('--opt', dest='opt', required=False)
 
@@ -18,17 +20,57 @@ def main():
 
     # OPTIONS !!! PUT THESE IN A CFG/TURN THESE INTO ARGUMENTS !!!
 
-    N = 2
-    SIM_DURATION = 20
+    N = 1
 
-    mass = 1
-    omega = 1
-    Lambda = 0.05
+    hbar = units.hbar
+
+
+    # Trap parametrs
+
+    #mass = 3.51e3 * 4/3 * np.pi * (5e-8)**3
+    #omega = 2 * np.pi * 1.5e5
+    mass = 1e-6
+
+    omega = 2 * np.pi * 1.5e5
+
+    spins = 1 # Defect denisties of 2e24 m^-3 reported in Kern et al PRB 2017
+
+    # Interaction strength. Appears in Hamiltonian as hbar*l*S_z*x
+    # (See Scala et al PRL 2013)
+    l = (0.015 * omega * spins) * np.sqrt(2 * mass * omega / hbar)
+
+    alpha = 2 * (2 / (mass * omega**2)) * hbar * l
+
     alpha_0 = 0
-    gamma = 5
 
-    resolution = 20*SIM_DURATION
-    error = 0.01
+
+    # Decoherence paramters
+
+    Gamma_sc = 1e4 # scattering events s^-1. See Romero-Isart PRA 2011 eq. 10
+
+    # off-diagonals decay as exp(-gamma*T*(x-x`)^2)
+    gamma = Gamma_sc * (2 * mass * omega / hbar)
+
+    T = omega / (4 * np.pi)
+
+    gamma_T = gamma * T
+
+
+    # Thermal parameters
+
+    occupancy = 65 # From Photon Recoil paper
+
+    beta = np.log((1/occupancy) + 1)/omega
+
+
+    # Simulation paramters
+
+    resolution = 64
+
+    error = 5e-4
+
+
+    # Coin operators
 
     Haddamard = (1/np.sqrt(2))*np.array([
         [1, 1],
@@ -42,16 +84,32 @@ def main():
 
     identity = np.eye(2)
 
+
+    # Initialisation
+
     COIN_OP = balanced_flip
 
-    # Calculate state after quantum walk
-    final_state = simulate.simulate(COIN_OP, SIM_DURATION, gamma)
+    walk_state = CoherentState(alpha_0, mass*omega)
+    #walk_state = ThermalState(beta, omega, mass)
 
-    # Calculate spatial pdf from final state
-    P_x, x = simulate.spatial_pdf(final_state, mass, omega, alpha_0, Lambda, resolution, error)
+    spin_state = SpinState(projector(2,0))
+
+    sample_points = np.linspace(-alpha, (N+1)*alpha, (N+3)*resolution)
+
+    method = final_state
+
+
+    # Simulation
+
+    final_pdf = [(method(N, x, x, 0, 0, walk_state,
+                              spin_state, COIN_OP, alpha, gamma_T, error)
+                  + method(N, x, x, 1, 1, walk_state,
+                                spin_state, COIN_OP, alpha, gamma_T, error))
+                 for x in sample_points]
 
     # Output
-    output.output(final_state, P_x)
+
+    output.output(sample_points, final_pdf)
 
 
 # Do not run if imported
--- cqo/output.py
+++ cqo/output.py
@@ -3,32 +3,13 @@ import numpy as np
 from algebra import condition_subsystem
 
 
-def output(state, P_x):
+def output(x, P_x):
 
     #! Create folder with time & date and key params
 
-    # Plot site probability distrubtions (up, down and total)
-    state_up, p_up = condition_subsystem(state, np.array([1,0]))
-    state_down, p_down = condition_subsystem(state, np.array([0,1]))
-
-    reduced_state = p_up * state_up + p_down * state_down
-
-    #! Put these in one figure
-    plt.figure()
-    plt.plot(np.diag(state_up))
-    plt.savefig("/home/matthewf/output_pdf_up.png")
-
-    plt.figure()
-    plt.plot(np.diag(state_down))
-    plt.savefig("/home/matthewf/output_pdf_down.png")
-
-    plt.plot(np.diag(reduced_state))
-    plt.savefig("/home/matthewf/output_pdf.png")
-
-
     #! Output graphical representation of final state
 
-    #! Plot spatial probability distribution
     plt.figure()
-    plt.plot(P_x)
-    plt.savefig("/home/matthewf/spatial_pdf.png")
+    plt.plot(x, P_x)
+    plt.show()
+    #plt.savefig("/home/matthewf/spatial_pdf.png")
