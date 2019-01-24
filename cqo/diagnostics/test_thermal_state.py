Fix normalisation of thermal state

cqo/diagnostics/test_thermal_state.py
@@ -2,7 +2,7 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 from itertools import product
-from cqo.simulation import ThermalState
+from cqo.simulation import ThermalState, CoherentState
 from cqo.units import hbar
 import numpy as np
 
@@ -10,25 +10,30 @@ import numpy as np
 
 output_folder = '/home/matthewf/PhD/programming/coined-quantum-oscillator/output'
 
-mass = 3.51e3 * 4/3 * np.pi * (5e-8)**3
-omega = 2 * np.pi * 1.5e5
-n = 10
-beta = np.log(1 + 1/n) / (omega*hbar)
+def beta(n, omega):
 
-rho = ThermalState(beta, omega, mass)
+    return np.log(1 + 1/n) / (omega*hbar)
+
+mass = 1#3.51e3 * 4/3 * np.pi * (5e-8)**3
+omega = 1#2 * np.pi * 1.5e5
+n = 0.001
+
+rho = ThermalState(beta(n, omega), omega, mass)
 
 print(rho.sample(0,0) / rho.sample(rho.width, rho.width))
 
 limit = 3*rho.width
 
-N = 4
+N = 3
+
+base_res = 32
 
 fidelity = np.zeros((N,))
 normalisation = np.zeros((N,))
 
 for i in range(1,N):
 
-    res = 16 * 2**i
+    res = base_res * 2**i
 
     x_array = np.linspace(-limit, limit, res)
 
@@ -60,5 +65,83 @@ fig2 = plt.figure()
 plt.plot(fidelity, normalisation)
 plt.ylim(ymin=0)
 plt.title('Norm vs resolution');
+
+# Normalisation vs temperature
+def norm(thermal_state):
+
+    limit = 3 * thermal_state.width
+
+    x_array = np.linspace(-limit, limit, 16)
+
+    pdf = [thermal_state.sample(x,x) for x in x_array]
+
+    return np.trapz(pdf, x_array)
+
+n_array = 10**np.arange(-6, 0.75, 0.25)
+
+beta_array = beta(n_array, omega)
+
+norm_array = [norm(ThermalState(b, omega, mass)) for b in beta_array]
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(1/beta_array, norm_array, label='Norm')
+plt.plot([0, 1/beta_array[-1]], [1, 1], '--')
+plt.ylim(ymin=0, ymax=1.1)
+plt.xlim(xmin=0)
+plt.title('Norm vs Temperature')
+plt.xlabel('1/beta')
+plt.legend()
+
+# Width vs temperature (classical high temperature asymptotics)
+
+width_array = [ThermalState(b, omega, mass).width for b in beta_array]
+
+c_width_array = [ThermalState(b, omega, mass).coherence_width for b in beta_array]
+
+high_t_width = np.sqrt(1 / (mass * omega**2 * beta_array))
+
+gs_width = np.sqrt(hbar/(2*mass*omega))
+
+plt.subplot(2,2,3)
+plt.plot(1/beta_array, width_array, label='QHO width')
+plt.plot(1/beta_array, high_t_width, label='CHO width')
+plt.plot(1/beta_array, c_width_array, label='Coherence length')
+plt.plot([0, 1/beta_array[-1]], [gs_width]*2, '--', label='Quantum width')
+plt.xlim(xmin=0)
+plt.title('Width vs Temperature')
+plt.xlabel('1/beta')
+plt.legend()
+
+plt.subplot(2,2,4)
+plt.plot(1/beta_array, width_array/high_t_width)
+plt.plot([0,1/beta_array[-1]], [1,1], '--')
+plt.xlim(xmin=0)
+plt.title('Classical width / Quantum Width')
+plt.xlabel('1/beta')
+
+# Comparison of low-T state with ground state
+
+b = 1e35
+
+low_t_state = ThermalState(b, omega, mass)
+
+x_array = np.linspace(-3*low_t_state.width, 3*low_t_state.width)
+
+gs_pdf = [CoherentState(0, mass * omega).sample(x, x) for x in x_array]
+
+low_t_pdf = [low_t_state.sample(x, x) for x in x_array]
+
+plt.figure()
+plt.plot(x_array, gs_pdf, '--', label='Ground State')
+plt.plot(x_array, low_t_pdf, label='Low T State')
+plt.plot([low_t_state.width]*2, [0, max(low_t_pdf)], '--')
+plt.legend()
+plt.title('Low T State vs Ground State')
+
+w = low_t_state.width
+exp_ratio = low_t_state.sample(0,0) / low_t_state.sample(w, w)
+
+print('Exponent: ', np.log(exp_ratio))
 
 plt.show()

cqo/main.py
@@ -30,13 +30,13 @@ def create_mesh(N, alpha, width, resolution):
 
     return mesh, sample_points
 
-def main(resolution = 7, run_expansion=False):
+def main(resolution = 10, run_expansion=False):
 
     logging.basicConfig(level = logging.DEBUG)
 
     ### Parameters ###
 
-    N = 6 # Walk steps
+    N = 4 # Walk steps
 
     hbar = units.hbar
 
@@ -65,9 +65,9 @@ def main(resolution = 7, run_expansion=False):
     Magnetic field gradient: 5e2 T m^-1
     (See Scala et al PRL 2013)
     """
+    dB_dz = 2e6
     g_NV = 2
     mu_B = 9.27e-24
-    dB_dz = 2e6
     F = g_NV * mu_B * dB_dz
 
     alpha = 2 * F / mass / omega**2
@@ -79,7 +79,7 @@ def main(resolution = 7, run_expansion=False):
     Decoherence rate: 2*pi*1.1e4 /s. See Romero-Isart PRA 2011 eq. 10
     off-diagonals decay as exp(-gamma*T*(x-x`)^2)
     """
-    Gamma_sc = 2 * np.pi * 0.15e3
+    Gamma_sc = 2 * np.pi * 1.15e0
 
     gamma = Gamma_sc / lscale**2
 
@@ -91,7 +91,7 @@ def main(resolution = 7, run_expansion=False):
     """
     Reported thermal occupancy: 65 phonons (From Photon Recoil paper)
     """
-    occupancy = 0 # 0.5, 2, 10, 50
+    occupancy = 20 # 0.5, 2, 10, 50
 
     if occupancy < 1e-3:
         walk_state = CoherentState(alpha_0, mass*omega)
@@ -130,7 +130,7 @@ def main(resolution = 7, run_expansion=False):
 
     COIN_OP = Hadamard
 
-    spin_state = SpinState(np.eye(2)) #SpinState(projector(2,1))
+    #spin_state = SpinState(np.eye(2)) #SpinState(projector(2,1))
 
     # Mesh
 
@@ -144,8 +144,8 @@ def main(resolution = 7, run_expansion=False):
 
     # Quantum Walk
 
-    quantum_state = [[[quantum_walk_state], []],
-                 [[], [quantum_walk_state]]]
+    quantum_state = [[[0.5*quantum_walk_state], []],
+                    [[], [0.5*quantum_walk_state]]]
 
     quantum_walk = MultiGaussianWalk(quantum_state, alpha, COIN_OP, gamma_T)
 
@@ -160,7 +160,10 @@ def main(resolution = 7, run_expansion=False):
                     quantum_walk.sample(x_vec[0], x_vec[1], s, s_) for s_ in [0,1]
                 ] for s in [0,1]
             ] for x_vec in mesh_row
-        ] for mesh_row in mesh])
+        ] for mesh_row in mesh
+    ])
+
+    # Classical walk
 
     classical_walk = ClassicalMGW(quantum_walk_state, alpha)
 
@@ -169,6 +172,8 @@ def main(resolution = 7, run_expansion=False):
     classical_pdf = np.array(
         [classical_walk.sample(x) for x in sample_points]
     )
+
+    # Free-flight expansion
 
     if run_expansion:
         # Evolve under free-flight
@@ -218,26 +223,36 @@ def main(resolution = 7, run_expansion=False):
             ] for mesh_row in mesh_final
         ])
 
-        #import pdb; pdb.set_trace()
-
 
     ### Output ###
 
+    # Sub-normalised PDFs conditioned on spin
     walk_0 = np.diag(rho_walk[:,:,0,0])
     walk_1 = np.diag(rho_walk[:,:,1,1])
 
+    # Full CW density matrix
     rho_walk_dm = np.block([
         [rho_walk[:,:,0,0], rho_walk[:,:,0,1]],
         [rho_walk[:,:,1,0], rho_walk[:,:,1,1]]])
 
     if run_expansion:
-
         final_0 = np.diag(rho_final_0_0)
         final_1 = np.diag(rho_final_1_1)
 
         print("Displacement: {}\nWidth: {}".format(alpha, walk_state.width))
 
         x_final = coords_final
+
+    # Check total probability for debugging purposes
+    walk_pdf = walk_0 + walk_1
+
+    total_P_quantum = np.trapz(walk_pdf, sample_points)
+
+    total_P_classical = np.trapz(classical_pdf, sample_points)
+
+    logging.debug('Total quantum probability: {}'.format(total_P_quantum))
+
+    logging.debug('Total classical probability: {}'.format(total_P_classical))
 
     output.draw_walk(sample_points, walk_0, walk_1, classical_pdf, "Walk PDF",
                       show=False)
@@ -246,7 +261,6 @@ def main(resolution = 7, run_expansion=False):
                                show=(not run_expansion))
 
     if run_expansion:
-
         output.draw_pdf(x_final, final_0, final_1, "Post-expansion PDF",
                           show=False)
 

cqo/simulation.py
@@ -20,6 +20,7 @@ def walk_amplitudes(N, coin_op, initial_state=[1,0]):
     R = N // 2 + N % 2
     L = N // 2
 
+    initial_state = np.array(initial_state, dtype = np.dtype(np.complex128))
     amplitudes = [initial_state]
     amplitudes = np.concatenate((amplitudes, [[0,0]]*R), axis=0)
     if L:
@@ -38,11 +39,11 @@ def walk_amplitudes(N, coin_op, initial_state=[1,0]):
         active_amplitudes = amplitudes[s:e,:].view()
         active_coin_op = coin_op_r[s:e,:,:].view()
 
-        # Coin op
+        # Apply the coin op
 
         active_amplitudes[:,:] = np.einsum('nij,nj->ni', active_coin_op, active_amplitudes)
 
-        # Shift R/L
+        # Apply the conditional shift
 
         if i % 2:
             # shift L
@@ -76,7 +77,8 @@ def final_state(N, x, x_, s, s_, walk_state, spin_state, coin_amplitudes, alpha,
         N (int): Number of steps in the quantum walk
         x, x_ (float): Spatial matrix element to evaluate
         s, s_ (int): Spin matrix element (s, s_ \in {0,1})
-        initial_state: Initial density matrix
+        walk_state: Initial walker state
+        spin_state: Initial coin SpinState
         coin_amplitudes (2x2 ndarray): Elements of the coin operator
         alpha (float): Magnitude of displacement
         decoherence_rate (float): Decoherence rate
@@ -203,10 +205,6 @@ class CoherentState:
 
     def state(self, x):
 
-        h = units.hbar
-
-        mw = self.m_omega
-
         a = self.alpha
 
         w = self.sigma
@@ -251,8 +249,16 @@ class ThermalState:
         else:
             self.Z = 1/(1-t)
 
-        self.N = np.sqrt(mass * omega / (np.pi * units.hbar * self.detA))
-        self.N *= 4/self.Z
+        # Normalisation of coherent state
+        dx = np.sqrt(2*units.hbar / (mass * omega))
+
+        self.N = np.sqrt(2 / (np.pi * dx**2))
+
+        # Evaluation of Gaussian integral
+        self.N *= 4 / np.sqrt(self.detA)
+
+        # Normalisation by partition function
+        self.N /= self.Z
 
         self.mw = mass * omega
 
@@ -266,15 +272,18 @@ class ThermalState:
 
     def _width(self):
 
+        # Change a from x, x' basis to X, \delta x
         H = np.array([
             [0.5, 0.5],
-            [0.5, -0.5]])
+            [1, -1]])
 
-        width_matrix = H @ self.a @ H
+        width_matrix = H @ self.a @ H.T
 
-        width = np.sqrt(0.5/width_matrix[0,0])
+        width = np.sqrt(1/(2*width_matrix[0,0]))
 
-        coherence_width = np.sqrt(0.5/width_matrix[1,1])
+        width /= 2
+
+        coherence_width = np.sqrt(1/(2*width_matrix[1,1]))
 
         return width, coherence_width
 
@@ -451,6 +460,7 @@ class MultiGaussianWalk:
 
     def _coin(self):
 
+        # Container for Coin-Walker state after coin update rule
         new_state = [[[], []],
                      [[], []]]
 