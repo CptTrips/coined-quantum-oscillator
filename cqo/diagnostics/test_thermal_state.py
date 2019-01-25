Fix normalisation of Gaussian class

cqo/diagnostics/test_thermal_state.py
@@ -20,8 +20,6 @@ n = 0.001
 
 rho = ThermalState(beta(n, omega), omega, mass)
 
-print(rho.sample(0,0) / rho.sample(rho.width, rho.width))
-
 limit = 3*rho.width
 
 N = 3
@@ -69,7 +67,7 @@ plt.title('Norm vs resolution');
 # Normalisation vs temperature
 def norm(thermal_state):
 
-    limit = 3 * thermal_state.width
+    limit = 5 * thermal_state.width
 
     x_array = np.linspace(-limit, limit, 16)
 
@@ -139,9 +137,22 @@ plt.plot([low_t_state.width]*2, [0, max(low_t_pdf)], '--')
 plt.legend()
 plt.title('Low T State vs Ground State')
 
-w = low_t_state.width
-exp_ratio = low_t_state.sample(0,0) / low_t_state.sample(w, w)
+# Attribute width vs calculated width
 
-print('Exponent: ', np.log(exp_ratio))
+def calculate_width(t):
+
+    exponent = np.log(t.sample(0,0) / t.sample(t.width, t.width))
+
+    return t.width * np.sqrt( 1 / (2 * exponent))
+
+thermal_state_array = [ThermalState(b, omega, mass) for b in beta_array]
+
+calculated_width_array = [calculate_width(t) for t in thermal_state_array]
+
+plt.figure()
+plt.plot(1/beta_array, calculated_width_array, label='Calculated width')
+plt.plot(1/beta_array, width_array, '--', 'Width attribute')
+plt.legend()
+plt.xlabel('1/beta')
 
 plt.show()

cqo/main.py
@@ -30,13 +30,13 @@ def create_mesh(N, alpha, width, resolution):
 
     return mesh, sample_points
 
-def main(resolution = 10, run_expansion=False):
+def main(resolution = 16, run_expansion=False):
 
     logging.basicConfig(level = logging.DEBUG)
 
     ### Parameters ###
 
-    N = 4 # Walk steps
+    N = 3 # Walk steps
 
     hbar = units.hbar
 
@@ -65,7 +65,7 @@ def main(resolution = 10, run_expansion=False):
     Magnetic field gradient: 5e2 T m^-1
     (See Scala et al PRL 2013)
     """
-    dB_dz = 2e6
+    dB_dz = 2e5
     g_NV = 2
     mu_B = 9.27e-24
     F = g_NV * mu_B * dB_dz
@@ -79,7 +79,7 @@ def main(resolution = 10, run_expansion=False):
     Decoherence rate: 2*pi*1.1e4 /s. See Romero-Isart PRA 2011 eq. 10
     off-diagonals decay as exp(-gamma*T*(x-x`)^2)
     """
-    Gamma_sc = 2 * np.pi * 1.15e0
+    Gamma_sc = 2 * np.pi * 1.15e0 * 0
 
     gamma = Gamma_sc / lscale**2
 
@@ -91,7 +91,7 @@ def main(resolution = 10, run_expansion=False):
     """
     Reported thermal occupancy: 65 phonons (From Photon Recoil paper)
     """
-    occupancy = 20 # 0.5, 2, 10, 50
+    occupancy = 10 # 0.5, 2, 10, 50
 
     if occupancy < 1e-3:
         walk_state = CoherentState(alpha_0, mass*omega)